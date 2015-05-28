7f7ab32f90b3005c4d758a989ab27cc56c506526
Joel Arvidsson <joel@oblador.se>
2015-05-28 18:15:15 +0200

    Make overswipes more native like by applying rubber band effect.

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ var Swipeout = React.createClass({
       height: 0
     , swipeoutMaxWidth: 0
     , swipeoutOpen: false
-    , swipeoutTimeStart: null
     , swiping: false
     , width: 0
     }
@@ -37,33 +36,20 @@ var Swipeout = React.createClass({
   }
 , _handlePanResponderGrant: function(e: Object, gestureState: Object) {
     this.refs.swipeoutContent.measure((ox, oy, width, height) => {
-      console.log('h: '+height+', w: '+width+', swipeoutMax: '+(width/5 - 4)*this.props.btns.length)
       this.setState({
         height: height,
         swiping: true,
-        swipeoutTimeStart: (new Date()).getTime(),
         swipeoutMaxWidth: (width/5 - 4)*this.props.btns.length,
         width: width
       })
     })
   }
 , _handlePanResponderMove: function(e: Object, gestureState: Object) {
-    var timeDiff = (new Date()).getTime() - this.state.swipeoutTimeStart
     var xMin = 0
     var xPos = gestureState.dx
     if (this.state.swipeoutOpen) var xPos = gestureState.dx - this.state.swipeoutMaxWidth
     if (this.state.swiping) {
-      if (timeDiff < 160 && xMin-xPos > 10) {
-        this.tweenState('contentLeft', {
-          easing: tweenState.easingTypes.easeInOutQuad,
-          duration: 200,
-          endValue: -this.state.swipeoutMaxWidth
-        })
-      } else if (xPos < 0) {
-        this.setState({ contentLeft: xPos })
-      } else {
-        this.setState({ contentLeft: 0 })
-      }
+      this.setState({ contentLeft: Math.min(xPos, xMin) })
     }
   }
 , _handlePanResponderEnd: function(e: Object, gestureState: Object) {
@@ -81,14 +67,20 @@ var Swipeout = React.createClass({
     }
     this.setState({ swiping: false })
   }
+, _rubberBandEasing: function(value, lowerLimit) {
+    if(value < lowerLimit) {
+      return lowerLimit - Math.pow(lowerLimit - value, 0.85);
+    } 
+    return value;
+  }
 , render: function() {
     var self = this
     var styleSwipeoutMove = StyleSheet.create({
       swipeoutBtns: {
-        left: Math.abs(this.state.width+this.getTweeningValue('contentLeft'))
+        left: Math.abs(this.state.width+ Math.max(this.state.swipeoutMaxWidth*-1, this.getTweeningValue('contentLeft')))
       },
       swipeoutContent: {
-        left: this.getTweeningValue('contentLeft')
+        left: this._rubberBandEasing(this.getTweeningValue('contentLeft'), this.state.swipeoutMaxWidth*-1)
       }
     })
     var styleSwipeoutContent = [styles.swipeoutContent]
