c2a6201b42f4666e9a2e76d46dca9fb1235ac9cf
Dan Cormier <dancormierall@gmail.com>
2015-10-10 17:04:02 -0400

    update for RN@0.12.0

---
+++ index.ios.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var Examples = require('./example/example.js')
+
+var reactnativeswipeout = React.createClass({
+  render: function() {
+    return (
+      <View>
+        <Examples/>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('reactnativeswipeout', () => reactnativeswipeout);
