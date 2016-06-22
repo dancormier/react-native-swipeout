3db15b7fe78aa6502b3f7a44a6fe9431cd7376a2
maranbomater <zoran@arsfutura.co>
2016-06-22 15:58:22 +0200

    Upgraded to support React Native 0.26.0 and above.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var React = require('react-native')
+var React = require('react');
+var ReactNative = require('react-native')
 var tweenState = require('react-tween-state')
-var {PanResponder, TouchableHighlight, StyleSheet, Text, View} = React
+var {PanResponder, TouchableHighlight, StyleSheet, Text, View} = ReactNative;
 var styles = require('./styles.js')
 
 var SwipeoutBtn = React.createClass({
