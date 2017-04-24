4a06e6aeaeed6ca1ce62979492653f32769aa92f
Adam Burdette <aburdette@marketamerica.com>
2017-04-24 18:23:35 -0400

    Added Manual Open
    Added a manual open that follows the current architecture for close.  By setting openRight or openLeft, componentWillReceiveProps will call the corresponding function and open the menu appropriately.

--- index.js
+++ index.js
@@ -148,6 +148,8 @@ const Swipeout = React.createClass({
 
   componentWillReceiveProps: function(nextProps) {
     if (nextProps.close) this._close();
+    if (nextProps.openRight) this._openRight();
+    if (nextProps.openLeft) this._openLeft();
   },
 
   _handlePanResponderGrant: function(e: Object, gestureState: Object) {
@@ -261,6 +263,45 @@ const Swipeout = React.createClass({
       openedRight: false,
       openedLeft: false,
     });
+  },
+
+  _openRight: function() {
+    return new Promise((resolve, reject) => {
+      this.refs.swipeoutContent.measure((ox, oy, width, height) => {
+        this.setState({
+          btnWidth: (width/5),
+          btnsRightWidth: this.props.right ? (width/5)*this.props.right.length : 0,
+        });
+        resolve();
+      });
+    }).then(() =>{
+      this._tweenContent('contentPos', -this.state.btnsRightWidth);
+      this.setState({ 
+        contentPos: -this.state.btnsRightWidth, 
+        openedLeft: false, 
+        openedRight: true, 
+        swiping: false 
+      });
+    })
+  },
+
+  _openLeft: function() {
+    return new Promise((resolve, reject) => {
+      this.refs.swipeoutContent.measure((ox, oy, width, height) => {
+        this.setState({
+          btnWidth: (width/5),
+          btnsLeftWidth: this.props.left ? (width/5)*this.props.left.length : 0,
+        });
+      });
+    }).then(() =>{
+      this._tweenContent('contentPos', -this.state.btnsRightWidth);
+      this.setState({ 
+        contentPos: this.state.btnsLeftWidth, 
+        openedLeft: true, 
+        openedRight: false, 
+        swiping: false 
+      });
+    })
   },
 
   render: function() {
