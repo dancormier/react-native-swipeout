2704e717a728c7c022b58d0ed06a5d1c2da9652f
jamesbillinger <jamesbillinger@users.noreply.github.com>
2017-05-11 02:36:31 -0500

    Allow to be nested within other swipeable components (#158)

--- index.js
+++ index.js
@@ -147,6 +147,7 @@ const Swipeout = React.createClass({
       onPanResponderRelease: this._handlePanResponderEnd,
       onPanResponderTerminate: this._handlePanResponderEnd,
       onShouldBlockNativeResponder: (event, gestureState) => false,
+      onPanResponderTerminationRequest: () => false,
     });
   },
 
