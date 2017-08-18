f427e158ddcca3711edaed9850d049f156be8f32
Adrian Hall <adrian@shellmonger.com>
2017-08-18 00:44:50 -0700

    Types for TypeScript 2.0 Support (#207)
    *feat:  Adding TypeScript 2.0 types

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+import React from 'react';
+
+declare module 'react-native-swipeout' {
+    export interface SwipeoutButtonProperties {
+        backgroundColor?: string;
+        color?: string;
+        component?: JSX.Element;
+        onPress?(): void;
+        text?: string;
+        type?: 'default'|'delete'|'primary'|'secondary';
+        underlayColor?: string;
+        disabled?: boolean;
+    }
+
+    export interface SwipeoutProperties {
+        autoClose?: boolean;
+        backgroundColor?: string;
+        close?: boolean;
+        disabled?: boolean;
+        left?: SwipeoutButtonProperties[];
+        onOpen?(sectionId: number, rowId: number, direction: string): void;
+        onClose?(sectionId: number, rowId: number, direction: string): void;
+        right?: SwipeoutButtonProperties[];
+        scroll?(): void;
+        style?: Object;
+        sensitivity?: number;
+        buttonWidth?: number;
+        rowId?: number;
+        sectionId?: number;
+    }
+
+    export default class Swipeout extends React.Component<SwipeoutProperties,any> {}
+}
