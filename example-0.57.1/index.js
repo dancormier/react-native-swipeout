1dbcdbdf31a40a77ea33dd43b73635a2e72fbfdd
Ryan O'Boril <ryan@arcweb.co>
2018-09-25 00:22:22 -0400

    cleanup

--- example-0.57.1/index.js
+++ example-0.57.1/index.js
@@ -1,7 +1,6 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-//import App from './App';
 import {name as appName} from './app.json';
 import SwipeoutExample from './SwipeoutExample';
 
