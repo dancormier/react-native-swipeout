42b28b32158b344850836e23337bc13f556eb4e1
Max Schmeling <max@maxschmeling.me>
2018-12-20 21:31:35 -0600

    Adds and exports ButtonType enum for type prop

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,20 @@
 import * as React from 'react';
 
 declare module 'react-native-swipeout' {
+    export enum ButtonType {
+        DEFAULT = "default",
+        DELETE = "delete",
+        PRIMARY = "primary",
+        SECONDARY = "secondary"
+    }
+
     export interface SwipeoutButtonProperties {
         backgroundColor?: string;
         color?: string;
         component?: JSX.Element;
         onPress?(): void;
         text?: React.ReactNode;
-        type?: 'default'|'delete'|'primary'|'secondary';
+        type?: ButtonType;
         underlayColor?: string;
         disabled?: boolean;
     }
